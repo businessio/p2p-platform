2f64789da7033998d11a6d890b5335291d85f8d2
Luciano Jr <luciano@lucianojr.com.br>
2019-08-20 20:42:51 +0100

    Only build folder

---
+++ precache-manifest.e9faa9d26cad1aeb42a1954d4a288065.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ad83994f01ca1d2cc3c5753e36cc52",
+    "url": "/p2p-platform/index.html"
+  },
+  {
+    "revision": "b9bf5fe3b0bdf378be99",
+    "url": "/p2p-platform/static/css/main.bd4f92f2.chunk.css"
+  },
+  {
+    "revision": "edfe5e150ea37f75cf7d",
+    "url": "/p2p-platform/static/js/2.4b805124.chunk.js"
+  },
+  {
+    "revision": "b9bf5fe3b0bdf378be99",
+    "url": "/p2p-platform/static/js/main.01ba5075.chunk.js"
+  },
+  {
+    "revision": "28016771f912acc9d687",
+    "url": "/p2p-platform/static/js/runtime~main.1a2bbffa.js"
+  }
+]);
